ce9856a69e9d0fe141c34a82bc21d1fb762f732d
GuG <tairan.gao.coding@gmail.com>
2023-05-23 15:39:04 -0400

    update requirement.txt

--- main.py
+++ main.py
@@ -2,14 +2,15 @@
 # @Author: Tairan Gao
 # @Date:   2023-05-22 21:52:44
 # @Last Modified by:   Tairan Gao
-# @Last Modified time: 2023-05-23 14:13:12
+# @Last Modified time: 2023-05-23 15:38:30
 
 from __future__ import annotations
+from pathlib import Path
 
 import asyncio
 
 from view import register_ws_route, ws, LiveChart, LiveChartView
-from datafeed import OHLCBarFeed
+from data_feed import OHLCBarFeed
 from data import OHLCData
 
 from engine import BackTestEngine as Engine
@@ -23,10 +24,12 @@ from strategy import DummyStrategy
 async def main():
     bus = EventBus()
 
+    base_dir = Path(__file__).resolve().parent
+    file_path = base_dir / "data_example" / "DAT_ASCII_USDCAD_M1_202304.csv"
     data_ohlc = OHLCData.from_csv_file(
-        Asset(AssetType.FX, "USDCAD"), "data_example/DAT_ASCII_USDCAD_M1_202304.csv"
+        Asset(AssetType.FX, "USDCAD"),
+        file_path,
     )
-
     feed = OHLCBarFeed(bus, OHLCData=data_ohlc, push_freq=1)
     live_chart = LiveChart()
     register_ws_route(live_chart.app, "/ws", ws, live_chart)
